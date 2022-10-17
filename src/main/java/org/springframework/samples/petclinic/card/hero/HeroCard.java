f59ab66582711b66c440632ce61a49ff6aa36116
Jesus Solis Ortega <jesussolisortega@gmail.com>
2022-10-17 16:56:51 +0200

    import error fix

--- src/main/java/org/springframework/samples/petclinic/card/hero/HeroCard.java
+++ src/main/java/org/springframework/samples/petclinic/card/hero/HeroCard.java
@@ -2,13 +2,15 @@ package org.springframework.samples.petclinic.card.hero;
 
 import javax.persistence.Entity;
 
+import org.springframework.samples.petclinic.card.Card;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class HeroCard {
+public class HeroCard extends Card{
     private Integer health;
     private Boolean isFinal;
     private Integer playersNecessary;
