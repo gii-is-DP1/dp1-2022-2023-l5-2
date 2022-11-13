b3d19c0159ce4b593ca3d3c2fb30a371a3388cf9
ElenaTomasVela <eletomvel@alum.us.es>
2022-11-13 11:50:52 +0100

    Fixed accident when merging that caused compilation failures

--- src/main/java/org/springframework/samples/bossmonster/game/player/Player.java
+++ src/main/java/org/springframework/samples/bossmonster/game/player/Player.java
@@ -2,11 +2,14 @@ package org.springframework.samples.bossmonster.game.player;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.samples.bossmonster.game.card.Card;
 import org.springframework.samples.bossmonster.model.BaseEntity;
 import org.springframework.samples.bossmonster.user.User;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,8 +21,8 @@ public class Player extends BaseEntity {
     private Integer health;
     private Integer souls;
 
-    // @OneToOne
-    // private List<Card> hand;
-    // private Dungeon dungeon;  //TODO
+    @OneToMany
+    private List<Card> hand;
+
 
 }
