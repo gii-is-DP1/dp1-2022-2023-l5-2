566aa884fa0b671cb8d9366dbd9827ef5b899c02
FRANCISCO_DE_ASÍS <franrosora@gmail.com>
2022-12-01 13:12:42 +0100

    Hechizos en base de datos

--- src/main/java/org/springframework/samples/bossmonster/game/card/EffectTarget.java
+++ src/main/java/org/springframework/samples/bossmonster/game/card/EffectTarget.java
@@ -3,10 +3,13 @@ package org.springframework.samples.bossmonster.game.card;
 //Enumerado actua como dummy para la base de datos.
 public enum EffectTarget {
     ENUMERADO,
-    ROOM,
     PLAYER,
+    ROOM,
     HEROINDUNGEON,
     HEROINTOWN,
     ALLPLAYERS,
+    MONSTERROOM,
+    TRAPROOM,
+    HEROINSCORE
 
 }
