2c5b03d743da9cc8c75fa05afcc402eef75b6f6a
ElenaTomásVela <eletomvel@alum.us.es>
2022-12-01 11:40:29 +0100

    Boss Monster repo and deck method in service

---
+++ src/main/java/org/springframework/samples/bossmonster/game/card/finalBoss/FinalBossCardRepository.java
@@ -0,0 +1,6 @@
+package org.springframework.samples.bossmonster.game.card.finalBoss;
+
+import org.springframework.samples.bossmonster.game.card.CardRepository;
+
+public interface FinalBossCardRepository extends CardRepository<FinalBossCard> {
+}
