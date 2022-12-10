131ff23462dc5a4312fa39068b606f8332ad7ffe
tcabgom <tadcabgom@alum.us.es>
2022-12-10 23:22:09 +0100

    New functions in Game class

--- src/main/java/org/springframework/samples/bossmonster/game/Game.java
+++ src/main/java/org/springframework/samples/bossmonster/game/Game.java
@@ -7,6 +7,7 @@ import org.springframework.samples.bossmonster.game.card.TreasureType;
 import org.springframework.samples.bossmonster.game.card.finalBoss.FinalBossCard;
 import org.springframework.samples.bossmonster.game.card.hero.HeroCard;
 import org.springframework.samples.bossmonster.game.card.room.RoomCard;
+import org.springframework.samples.bossmonster.game.card.room.RoomType;
 import org.springframework.samples.bossmonster.game.card.spell.SpellCard;
 import org.springframework.samples.bossmonster.game.dungeon.Dungeon;
 import org.springframework.samples.bossmonster.game.dungeon.DungeonRoomSlot;
@@ -19,6 +20,7 @@ import javax.persistence.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -48,7 +50,6 @@ public class Game extends BaseEntity {
 
     @OneToMany
     private List<FinalBossCard> finalBossPile;
-
 
     @OneToMany
     private List<HeroCard> city;
@@ -102,13 +103,21 @@ public class Game extends BaseEntity {
     public void refillRoomPile() {
         for(Card card: discardPile) {
             if (card.getClass() == RoomCard.class) {
-                // I gave up
+                roomPile.add((RoomCard) card);
+                discardPile.remove(card);
             }
         }
+        Collections.shuffle(roomPile);
     }
 
     public void refillSpellPile() {
-
+        for(Card card: discardPile) {
+            if (card.getClass() == SpellCard.class) {
+                spellPile.add((SpellCard) card);
+                discardPile.remove(card);
+            }
+        }
+        Collections.shuffle(spellPile);
     }
 
     ////////// HERO RELATED //////////
@@ -158,28 +167,58 @@ public class Game extends BaseEntity {
 
     ////////// DUNGEON RELATED //////////
 
-    public void checkPlaceableRoomInDungeonPosition(Player player, Integer position, RoomCard room) {
-
+    public Boolean checkPlaceableRoomInDungeonPosition(Player player, Integer position, RoomCard room) {
+        Boolean result;
+        RoomType oldRoomType = player.getDungeon().getRoomSlots()[position].getRoom().getRoomType();
+        RoomType newRoomType = room.getRoomType();
+        switch (newRoomType) {
+            case ADVANCED_MONSTER: { result = oldRoomType == RoomType.MONSTER; break; }
+            case ADVANCED_TRAP: { result = oldRoomType == RoomType.TRAP; break; }
+            default: result = true;
+        }
+        return result;
     }
 
-    public void placeDungeonRoom(Player player, Integer position, RoomCard room) {
-        Dungeon playerDungeon = player.getDungeon();
-        DungeonRoomSlot[] slots = playerDungeon.getRoomSlots();
-        slots[position].setRoom(room);
-        player.setDungeon(playerDungeon);
+    // Probablemente no funcione
+    public Boolean placeDungeonRoom(Player player, Integer position, RoomCard room) {
+        Boolean placed;
+        if (checkPlaceableRoomInDungeonPosition(player, position, room)) {
+            Dungeon playerDungeon = player.getDungeon();
+            DungeonRoomSlot[] slots = playerDungeon.getRoomSlots();
+            slots[position].setRoom(room);
+            player.setDungeon(playerDungeon);
+            placed = true;
+        }
+        else placed = false;
+        return placed;
     }
 
     public void destroyDungeonRoom(Player player, Integer position) {
-        // Almost the same that the last one. I still made it to make the code easier to understand
         Dungeon playerDungeon = player.getDungeon();
         DungeonRoomSlot[] slots = playerDungeon.getRoomSlots();
         slots[position].setRoom(null);
         player.setDungeon(playerDungeon);
     }
 
-    public void heroAdvanceRoomDungeon() {
+    public void heroAdvanceRoomDungeon(Player player) {
+        Dungeon playerDungeon = player.getDungeon();
+        for(int i = 4; i >= 0; i --) {
+            DungeonRoomSlot room = playerDungeon.getRoomSlots()[i];
+            Integer dealtDamage = room.getRoomTrueDamage();
+            for(HeroCard hero: room.getHeroesInRoom()) {
+                //
+            }
+        }
+    }
+
+    public void heroAutomaticallyMovesAfterDestroyingRoom() {
 
     }
 
+    ////////// MISC //////////
+
+    public void sortPlayersByFinalBossEx() {
+        setPlayers(players.stream().sorted(Comparator.comparing(x -> x.getDungeon().getBossCard().getXp(), Comparator.reverseOrder())).collect(Collectors.toList()));
+    }
 
 }
