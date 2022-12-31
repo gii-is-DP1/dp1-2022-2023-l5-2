b814e7afd317794a899828233f18648e6cef3618
Jesus Solis Ortega <jesussolisortega@gmail.com>
2022-12-31 11:10:07 +0100

    deck creation tests added

--- src/test/java/org/springframework/samples/bossmonster/game/CardServiceTest.java
+++ src/test/java/org/springframework/samples/bossmonster/game/CardServiceTest.java
@@ -1,12 +1,17 @@
 package org.springframework.samples.bossmonster.game;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.bossmonster.game.card.CardService;
+import org.springframework.samples.bossmonster.game.card.finalBoss.FinalBossCard;
 import org.springframework.samples.bossmonster.game.card.hero.HeroCard;
+import org.springframework.samples.bossmonster.game.card.room.RoomCard;
+import org.springframework.samples.bossmonster.game.card.spell.SpellCard;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
@@ -18,6 +23,7 @@ public class CardServiceTest {
     CardService cardService;
 
     @Test
+    @DisplayName("create Hero card Deck")
     public void shouldCreateHeroDeck() {
         List<HeroCard> deck = cardService.createHeroCardDeck();
         List<HeroCard> deck2 = cardService.createHeroCardDeck();
@@ -25,4 +31,35 @@ public class CardServiceTest {
         assertTrue(deck.get(0) instanceof HeroCard);
         assertNotEquals(deck,deck2);
     }
+
+    @Test
+    @DisplayName("create Spell card Deck")
+    public void shouldCreateSpellDeck() {
+        List<SpellCard> deck = cardService.createSpellCardDeck();
+        List<SpellCard> deck2 = cardService.createSpellCardDeck();
+        assertFalse(deck.isEmpty());
+        assertTrue(deck.get(0) instanceof SpellCard);
+        assertNotEquals(deck,deck2);
+    }
+
+    @Test
+    @DisplayName("create Room card Deck")
+    public void shouldCreateRoomDeck() {
+        List<RoomCard> deck = cardService.createRoomCardDeck();
+        List<RoomCard> deck2 = cardService.createRoomCardDeck();
+        assertFalse(deck.isEmpty());
+        assertTrue(deck.get(0) instanceof RoomCard);
+        assertNotEquals(deck,deck2);
+    }
+
+    @Test
+    @DisplayName("create Boss card Deck")
+    public void shouldCreateBossDeck() {
+        List<FinalBossCard> deck = cardService.createBossCardDeck();
+        List<FinalBossCard> deck2 = cardService.createBossCardDeck();
+        assertFalse(deck.isEmpty());
+        assertTrue(deck.get(0) instanceof FinalBossCard);
+        assertNotEquals(deck,deck2);
+    }
+
 }
