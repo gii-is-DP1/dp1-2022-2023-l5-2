8f6e959f8bb240af57c73a00571de591d57237f8
Jesus Solis Ortega <jesussolisortega@gmail.com>
2023-01-04 17:05:37 +0100

    StatisticsControllerTests Done

--- src/main/java/org/springframework/samples/bossmonster/statistics/StatisticsController.java
+++ src/main/java/org/springframework/samples/bossmonster/statistics/StatisticsController.java
@@ -55,8 +55,8 @@ public class StatisticsController {
         result.addObject("gamesResult", games);
 
         return result;
-
     }
+
     @GetMapping("/users/statistics/global")
     public ModelAndView showGlobalStatistics(){
         ModelAndView result= new ModelAndView(GLOBAL_STATISTICS_VIEW);
@@ -83,6 +83,7 @@ public class StatisticsController {
 
         return result;
     }
+
     @GetMapping("/users/statistics/rankings/winRate")
     public ModelAndView rankingWinrate(){
         ModelAndView result= new ModelAndView(RANKING_WINRATE_VIEW);
@@ -90,6 +91,7 @@ public class StatisticsController {
         result.addObject("ranking", ranking);
         return result;
     }
+
     @GetMapping("/users/statistics/rankings/wins")
     public ModelAndView rankingWins(){
         ModelAndView result= new ModelAndView(RANKING_WINS_VIEW);
@@ -99,7 +101,7 @@ public class StatisticsController {
     }
 
     @GetMapping("/statistics/listPlayedGames")
-    public ModelAndView show(){
+    public ModelAndView showPlayedGames(){
         ModelAndView result= new ModelAndView(PLAYED_GAMES);
         result.addObject("playedGames", service.findAllGames());
         return result;
@@ -114,4 +116,5 @@ public class StatisticsController {
 
         return result;
     }
+
 }
