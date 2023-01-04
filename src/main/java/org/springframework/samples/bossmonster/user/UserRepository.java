0ccd09b6ebe41f9022ac85086eaabc2aec0c781e
FRANCISCO_DE_ASÍS <franrosora@gmail.com>
2023-01-04 11:54:59 +0100

    Welcome que se rompe siempre

--- src/main/java/org/springframework/samples/bossmonster/user/UserRepository.java
+++ src/main/java/org/springframework/samples/bossmonster/user/UserRepository.java
@@ -2,11 +2,14 @@ package org.springframework.samples.bossmonster.user;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.repository.CrudRepository;
 
 
-public interface UserRepository extends  CrudRepository<User, String>{
+public interface UserRepository extends CrudRepository<User, String>{
     
+    Page<User> findAllPaged(Pageable pageable);
 
     List<User> findAll();
 
