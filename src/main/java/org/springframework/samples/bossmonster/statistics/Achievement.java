e38e9bd041476f8f82258808a870aeb597d96364
Ignacio Gonzalez <igngongon2@alum.us.es>
2023-01-06 21:59:17 +0100

    Test terminados* modulo logros

--- src/main/java/org/springframework/samples/bossmonster/statistics/Achievement.java
+++ src/main/java/org/springframework/samples/bossmonster/statistics/Achievement.java
@@ -3,6 +3,7 @@ package org.springframework.samples.bossmonster.statistics;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -20,6 +21,7 @@ public class Achievement extends NamedEntity{
 
     private String image;
 
+    @Min(0)
     private Integer threshold;
 
     @Enumerated(EnumType.STRING)
